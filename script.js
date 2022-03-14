51de89943bf2657b67ba57711356b55415b47ea1
saifulalamfahime <you@example.com>
2022-03-14 16:07:26 +0600

    create some element with javascript

--- script.js
+++ script.js
@@ -7,17 +7,23 @@ const toDoList = document.querySelector(".list");
 
 btn.addEventListener('click', function(){
     console.log("kaj kore vai");
-
+// for div
     const newRow = document.createElement("div");
-
+// for h3
     const inputText = document.createElement("h3");
     inputText.innerHTML = input.value;
     newRow.appendChild(inputText);
+// complete button
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = "Complete";
+    newRow.appendChild(completeBtn);
+
+// remove button
+    const removeBtn = document.createElement("button");
+    removeBtn.innerHTML = "Remove";
+    newRow.appendChild(removeBtn);
 
 
-
-
-
-
+    
     toDoList.appendChild(newRow);
 })
